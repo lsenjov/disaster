dbe9d3a80269ad5f5951337bb1dd86b7cbcbef01
Logan Senjov <logansenjov@gmail.com>
2020-02-01 21:40:01 +1000

    Make sure test-config exists if it doesn't

--- src/clj/disaster/config.clj
+++ src/clj/disaster/config.clj
@@ -4,6 +4,12 @@
     [cprop.source :as source]
     [mount.core :refer [args defstate]]))
 
+;; Kinda dirty, but we need to make sure test-config.edn exists if it doesn't
+(try (slurp "test-config.edn")
+     (catch Exception e
+       (spit "test-config.edn"
+             (pr-str {}))))
+
 (defstate env
   :start
   (load-config
